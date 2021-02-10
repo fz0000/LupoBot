a8931fbf3039ba941d37963657c518328633f9b0
Nick <mail.nickkel@gmail.com>
2021-02-10 23:47:17 +0100

    Remove debug message

--- lupobot-plugins/lupobot-plugin-fun/src/main/java/de/nickkel/lupobot/plugin/fun/game/TicTacToeGame.java
+++ lupobot-plugins/lupobot-plugin-fun/src/main/java/de/nickkel/lupobot/plugin/fun/game/TicTacToeGame.java
@@ -75,7 +75,6 @@ public class TicTacToeGame {
             }
 
             if(end) {
-                System.out.println("End!");
                 this.message.editMessage(builder.build()).queue();
                 return;
             }
