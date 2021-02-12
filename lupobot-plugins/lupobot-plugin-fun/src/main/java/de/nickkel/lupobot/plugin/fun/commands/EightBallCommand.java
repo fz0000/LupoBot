524393ffc7d29321163c7ea9f7f627fd91a9a12b
Nick <mail.nickkel@gmail.com>
2021-02-13 00:55:15 +0100

    Add userId to title

--- lupobot-plugins/lupobot-plugin-fun/src/main/java/de/nickkel/lupobot/plugin/fun/commands/EightBallCommand.java
+++ lupobot-plugins/lupobot-plugin-fun/src/main/java/de/nickkel/lupobot/plugin/fun/commands/EightBallCommand.java
@@ -14,7 +14,7 @@ public class EightBallCommand extends LupoCommand {
         if(context.getArgs().length >= 1) {
             EmbedBuilder builder = new EmbedBuilder();
             builder.setColor(LupoColor.AQUA.getColor());
-            builder.setAuthor(context.getMember().getUser().getAsTag(), null, context.getMember().getUser().getAvatarUrl());
+            builder.setAuthor(context.getMember().getUser().getAsTag() + " (" + context.getMember().getId() + ")", null, context.getMember().getUser().getAvatarUrl());
             builder.addField(context.getServer().translate(context.getPlugin() ,"fun_eightball-question"), context.getArgsAsString(), false);
             builder.addField(context.getServer().translate(context.getPlugin() ,"fun_eightball-answer"),
                     context.getPlugin().getLanguageHandler().getRandomTranslation(context.getServer().getLanguage(), "fun_eightball-answer"), false);
