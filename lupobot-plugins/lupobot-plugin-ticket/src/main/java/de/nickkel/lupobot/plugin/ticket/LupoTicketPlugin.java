26bc857efdfeed17b942358d265e066717749bf2
Nick <mail.nickkel@gmail.com>
2021-02-23 00:05:13 +0100

    Create ticket plugin

---
+++ lupobot-plugins/lupobot-plugin-ticket/src/main/java/de/nickkel/lupobot/plugin/ticket/LupoTicketPlugin.java
@@ -0,0 +1,18 @@
+package de.nickkel.lupobot.plugin.ticket;
+
+import de.nickkel.lupobot.core.plugin.LupoPlugin;
+import de.nickkel.lupobot.core.plugin.PluginInfo;
+
+@PluginInfo(name = "ticket", version = "1.0.0", author = "Nickkel")
+public class LupoTicketPlugin extends LupoPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
