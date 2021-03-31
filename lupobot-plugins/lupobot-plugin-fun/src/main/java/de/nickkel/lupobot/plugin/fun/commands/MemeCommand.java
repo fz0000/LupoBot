105390f640e4bc3c10f13b6e8d218f70e7076e36
Nick <mail.nickkel@gmail.com>
2021-03-31 21:49:36 +0200

    Improve fun plugin

--- lupobot-plugins/lupobot-plugin-fun/src/main/java/de/nickkel/lupobot/plugin/fun/commands/MemeCommand.java
+++ lupobot-plugins/lupobot-plugin-fun/src/main/java/de/nickkel/lupobot/plugin/fun/commands/MemeCommand.java
@@ -9,22 +9,32 @@ import de.nickkel.lupobot.core.config.Document;
 import de.nickkel.lupobot.core.util.LupoColor;
 import net.dv8tion.jda.api.EmbedBuilder;
 
+import java.io.IOException;
+
 @CommandInfo(name = "meme", category = "general", cooldown = 5)
 public class MemeCommand extends LupoCommand {
     @Override
     public void onCommand(CommandContext context) {
-        Document config = new Document(("https://apis.duncte123.me/meme?nsfw=false"));
-        if(!config.getBoolean("success")) {
-            context.getChannel().sendMessage(context.getServer().translate(context.getPlugin(), "fun_meme-requests")).queue();
-            LupoBot.getInstance().getLogger().error("Error using meme command: " + config.convertToJsonString());
-        } else {
-            JsonObject jsonObject = config.getJsonElement("data").getAsJsonObject();
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.setColor(LupoColor.ORANGE.getColor());
-            builder.setTitle(jsonObject.get("title").getAsString(), jsonObject.get("url").getAsString());
-            builder.setImage(jsonObject.get("image").getAsString());
-            builder.setFooter(context.getServer().translate(context.getPlugin(), "fun_meme-footer"));
-            context.getChannel().sendMessage(builder.build()).queue();
+        try {
+            Document document = new Document(("https://apis.duncte123.me/meme?nsfw=false"));
+            if(!document.getBoolean("success")) {
+                failure(context);
+            } else {
+                JsonObject jsonObject = document.getJsonElement("data").getAsJsonObject();
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setColor(LupoColor.ORANGE.getColor());
+                builder.setTitle(jsonObject.get("title").getAsString(), jsonObject.get("url").getAsString());
+                builder.setImage(jsonObject.get("image").getAsString());
+                builder.setFooter(context.getServer().translate(context.getPlugin(), "fun_meme-footer"));
+                context.getChannel().sendMessage(builder.build()).queue();
+            }
+        } catch(Exception e) {
+            failure(context);
         }
+
+    }
+
+    private void failure(CommandContext context) {
+        context.getChannel().sendMessage(context.getServer().translate(context.getPlugin(), "fun_meme-requests")).queue();
     }
 }
