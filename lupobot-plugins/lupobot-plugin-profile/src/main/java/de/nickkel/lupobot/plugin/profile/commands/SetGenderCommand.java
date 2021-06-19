7896d0821538a9c6d61c8ae09bd0e73fdb007944
Nick <mail.nickkel@gmail.com>
2021-06-19 18:33:46 +0200

    Migrate profile commands

--- lupobot-plugins/lupobot-plugin-profile/src/main/java/de/nickkel/lupobot/plugin/profile/commands/SetGenderCommand.java
+++ lupobot-plugins/lupobot-plugin-profile/src/main/java/de/nickkel/lupobot/plugin/profile/commands/SetGenderCommand.java
@@ -3,23 +3,34 @@ package de.nickkel.lupobot.plugin.profile.commands;
 import de.nickkel.lupobot.core.command.CommandContext;
 import de.nickkel.lupobot.core.command.CommandInfo;
 import de.nickkel.lupobot.core.command.LupoCommand;
+import de.nickkel.lupobot.core.command.SlashOption;
 import de.nickkel.lupobot.core.util.LupoColor;
 import de.nickkel.lupobot.plugin.profile.LupoProfilePlugin;
 import de.nickkel.lupobot.plugin.profile.data.Profile;
 import de.nickkel.lupobot.plugin.profile.enums.Gender;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 @CommandInfo(name = "setgender", category = "general")
+@SlashOption(name = "gender", type = OptionType.STRING, choices = {"MALE", "FEMALE", "DIVERSE"})
 public class SetGenderCommand extends LupoCommand {
 
     @Override
     public void onCommand(CommandContext context) {
         if (context.getArgs().length == 1) {
+            String arg;
+            if (context.getSlash() == null) {
+                arg = context.getArgs()[0];
+            } else {
+                arg = context.getSlash().getOption("gender").getAsString();
+            }
+
             Profile profile = LupoProfilePlugin.getInstance().getProfile(context.getMember());
             Gender gender = null;
 
             for (Gender all : Gender.values()) {
-                if (all.toString().equalsIgnoreCase(context.getArgs()[0])) {
+                if (all.toString().equalsIgnoreCase(arg)) {
                     gender = all;
                 }
              }
@@ -34,11 +45,16 @@ public class SetGenderCommand extends LupoCommand {
                     .setColor(LupoColor.GREEN.getColor())
                     .setAuthor(context.getMember().getUser().getAsTag() + " (" + context.getMember().getId() + ")", null,
                             context.getMember().getUser().getAvatarUrl())
-                    .setTimestamp(context.getMessage().getTimeCreated())
+                    .setTimestamp(context.getTime())
                     .setDescription(context.getServer().translate(context.getPlugin(), "profile_setgender-success", gender.toString()));
-            context.getChannel().sendMessage(builder.build()).queue();
+            send(context, builder);
         } else {
             sendHelp(context);
         }
+    }
+
+    @Override
+    public void onSlashCommand(CommandContext context, SlashCommandEvent slash) {
+        onCommand(context);
     }
 }
--- lupobot-plugins/lupobot-plugin-profile/src/main/java/de/nickkel/lupobot/plugin/profile/commands/SetStatusCommand.java
+++ lupobot-plugins/lupobot-plugin-profile/src/main/java/de/nickkel/lupobot/plugin/profile/commands/SetStatusCommand.java
@@ -3,19 +3,29 @@ package de.nickkel.lupobot.plugin.profile.commands;
 import de.nickkel.lupobot.core.command.CommandContext;
 import de.nickkel.lupobot.core.command.CommandInfo;
 import de.nickkel.lupobot.core.command.LupoCommand;
+import de.nickkel.lupobot.core.command.SlashOption;
 import de.nickkel.lupobot.core.util.LupoColor;
 import de.nickkel.lupobot.plugin.profile.LupoProfilePlugin;
 import de.nickkel.lupobot.plugin.profile.data.Profile;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 @CommandInfo(name = "setstatus", category = "general")
+@SlashOption(name = "status", type = OptionType.STRING)
 public class SetStatusCommand extends LupoCommand {
 
     @Override
     public void onCommand(CommandContext context) {
         if (context.getArgsAsString().length() <= 30) {
             Profile profile = LupoProfilePlugin.getInstance().getProfile(context.getMember());
-            String newStatus = context.getArgsAsString();
+            String newStatus;
+            if (context.getSlash() == null) {
+                newStatus = context.getArgsAsString();
+            } else {
+                newStatus = context.getSlash().getOption("status").getAsString();
+            }
+
             if (newStatus.length() == 0) {
                 newStatus = "/";
             }
@@ -29,13 +39,18 @@ public class SetStatusCommand extends LupoCommand {
                     .setColor(LupoColor.GREEN.getColor())
                     .setAuthor(context.getMember().getUser().getAsTag() + " (" + context.getMember().getId() + ")", null,
                             context.getMember().getUser().getAvatarUrl())
-                    .setTimestamp(context.getMessage().getTimeCreated())
+                    .setTimestamp(context.getTime())
                     .setDescription(context.getServer().translate(context.getPlugin(), "profile_setstatus-success"))
                     .addField(context.getServer().translate(context.getPlugin(), "profile_setstatus-old"), oldStatus, false)
                     .addField(context.getServer().translate(context.getPlugin(), "profile_setstatus-new"), newStatus, false);
-            context.getChannel().sendMessage(builder.build()).queue();
+            send(context, builder);
         } else {
             sendSyntaxError(context, "profile_setstatus-too-long");
         }
+    }
+
+    @Override
+    public void onSlashCommand(CommandContext context, SlashCommandEvent slash) {
+        onCommand(context);
     }
 }
